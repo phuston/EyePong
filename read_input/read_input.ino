6fc8e15bc1ee7ae29b1f20a1694a6497584afa1d
unknown <phuston@7NMFG12.olin.edu>
2014-12-12 13:54:28 -0500

    Adds delay for improved response

--- read_input/read_input.ino
+++ read_input/read_input.ino
@@ -1,24 +1,25 @@
-int hashPin = 7;
-int delayTime = 20;
-
 void setup()
 {
   Serial.begin(9600);
-  pinMode(hashPin, INPUT);
+
 }
 
 void loop()
 {
-  int l_trigger = -1;
-  int r_trigger = 1;
+  int r_trigger = 400;
+  int l_trigger = 625;
+  int eyeIn = analogRead(A5);
   
-  int eyeIn = digitalRead(hashPin);
-  
-  if(eyeIn < l_trigger){
-    Serial.println(1);
-  }
-  else if(eyeIn > r_trigger){
+//  Serial.println(eyeIn);
+  if(eyeIn < l_trigger && eyeIn > r_trigger){
     Serial.println(0);
   }
+  if(eyeIn > l_trigger){
+    Serial.println(1);
+  }
+  else if(eyeIn < r_trigger){
+    Serial.println(-1);
+  }
+  delay(100);
 }
 
--- wrapper.py
+++ wrapper.py
@@ -2,21 +2,28 @@ from subprocess import Popen, PIPE
 import serial
 from time import sleep
 
+ser = serial.Serial(port='COM11', baudrate=9600) #Create connection with Arduino
+
 serOut = ser.readline()
 
-control_left_sequence = '''keydown Control_L
-key F4
-keyup Control_L
+control_left_sequence = '''key A
+key S
+key S
 '''
 
-shift_right_sequence = '''keydown Shift_L
-key A
-keyup Shift_L
+control_right_sequence = '''key T 
+key I 
+key T
+key S
 '''
 
 def keypress(sequence):
     p = Popen(['xte'], stdin=PIPE)
     p.communicate(input=sequence)
 
-keypress(shift_a_sequence)
-keypress(control_f4_sequence)
+while True:
+	serOut = ser.readline()
+	if(serOut == "Left"):
+		keypress(control_left_sequence)
+	elif(serOut == "Right"):
+		keypress(control_right_sequence)
